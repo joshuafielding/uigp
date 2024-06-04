75a9b433fe88c9ffab0ba25f6675877aaabb5a8e
josh fielding <joshfielding@outlook.com>
2024-06-04 11:49:56 -0400

    added temp homescreen, added xp and gold, changed layout

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let healthbar = 25;
 let previoushealthbar = 25;
-let playerdamage = 1;
+let playerdamage = 15;
 let saveplayerdamage;
-let kills = 0;
 let critchance = 1;
+let gold = 0;
+let goldgain;
+let xp = 0;
 
 //enemy array, can create multiple for differnt areas.
 const enemies = [
@@ -31,7 +33,6 @@ function calccritchance(){
     }
 }
 
-
 //the clicking part of the game :D
 document.getElementById("enemysprite").onclick = function() {
     if (healthbar > 0) {
@@ -40,7 +41,7 @@ document.getElementById("enemysprite").onclick = function() {
 
         //if the critical hit chance succeeds, we multiply the playerdamage variable
         if(crithit == true){
-            document.getElementById("crittext").style.display = "inline";
+            document.getElementById("crittext").style.visibility = "visible";
             saveplayerdamage = playerdamage;
             playerdamage *= 3.5;
             healthbar -= playerdamage;
@@ -48,23 +49,34 @@ document.getElementById("enemysprite").onclick = function() {
         }
         else{
             healthbar -= playerdamage;
-            document.getElementById("crittext").style.display = "none";
+            document.getElementById("crittext").style.visibility = "hidden";
         }
 
         //if the healthbar hits 0 it won't display negative values
-        if (healthbar < 0) {
-            document.getElementById("enemyhealth").textContent = "0";
+        if (healthbar <= 0) {
+            
+            //wasn't sure how to scale the health so i just have this for now. 
+            previoushealthbar *= 1.25;
+            healthbar = previoushealthbar;
+
+            //allows for image change
+            const randomenemy = getrandomenemy();
+            document.getElementById("enemysprite").src = randomenemy;
+            document.getElementById("enemyhealth").textContent = Math.trunc(healthbar);
+
+            //player gains gold
+            goldgain = Math.trunc(Math.random()*6-1)+1;
+            gold += goldgain
+            document.getElementById("gold").textContent = gold;
+            document.getElementById("goldgain").textContent = "Gold gained: +" + goldgain;
+
+            //player gains xp
+            xp += Math.trunc(Math.random()*3-1)+1;
+            document.getElementById("xp").textContent = xp;
         } else {
             document.getElementById("enemyhealth").textContent = Math.trunc(healthbar);
         }  
-    } else {
+    }/* else {
 
-        //wasn't sure how to scale the health so i just have this for now. 
-        previoushealthbar *= 1.25;
-        healthbar = previoushealthbar;
-
-        //allows for image change
-        const randomenemy = getrandomenemy();
-        document.getElementById("enemysprite").src = randomenemy;
-    }
+    }*/
 }
